1a7e39a9ec2020f13b2894c9d5502e31f3580aa0
dorothyynwong <dorothyynwong@gmail.com>
2024-12-03 08:57:04 +0000

    refactored Program.cs, added logger info while application has started

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,50 +1,64 @@
 using NLog;
+using NLog.Config;
+using NLog.Targets;
 using RedingtonCalculator.Enums;
 using RedingtonCalculator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-NLog.ILogger Logger = LogManager.GetCurrentClassLogger();
-string currentDirectory = Directory.GetCurrentDirectory();
-
-var config = new NLog.Config.LoggingConfiguration();
-
-var fileTarget = new NLog.Targets.FileTarget("logfile")
-{
-    FileName = Path.Combine(currentDirectory, "Logs/logfile.txt"),
-    Layout = "${longdate} ${level} ${message} ${exception}"
-};
-
-config.AddTarget(fileTarget);
-config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, fileTarget);
-LogManager.Configuration = config;
-
-builder.Services.AddSingleton<ICalculatorService<ProbabilityOperation>, ProbabilityService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins(builder.Configuration["Cors:Frontend"]!).AllowAnyMethod()
-                                                                    .AllowAnyHeader()
-                                                                    .AllowCredentials();
-    });
-});
-
-builder.Services.AddControllers();
+ConfigureLogging();
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors();
-
-app.MapControllers();
+ConfigureMiddleware(app);
+var logger = LogManager.GetCurrentClassLogger();
+logger.Info("Application has started.");
 
 app.Run();
+
+void ConfigureLogging()
+{
+    string currentDirectory = Directory.GetCurrentDirectory();
+    var config = new LoggingConfiguration();
+
+    var fileTarget = new FileTarget("logfile")
+    {
+        FileName = Path.Combine(currentDirectory, "Logs/logfile.txt"),
+        Layout = "${longdate} ${level} - ${logger}: ${message} ${exception}"
+    };
+
+    config.AddTarget(fileTarget);
+    config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, fileTarget);
+    LogManager.Configuration = config;
+}
+
+void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddSingleton<ICalculatorService<ProbabilityOperation>, ProbabilityService>();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+    services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(policy =>
+        {
+            policy.WithOrigins(configuration["Cors:Frontend"]!)
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials();
+        });
+    });
+
+    services.AddControllers();
+}
+
+void ConfigureMiddleware(WebApplication app)
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseCors();
+    app.MapControllers();
+}
