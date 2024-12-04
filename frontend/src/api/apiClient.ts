e4c720718cf26b14d6a6ab6fc0fc6673da5b3539
dorothyynwong <dorothyynwong@gmail.com>
2024-12-04 10:47:07 +0000

    changed timeout in apiclient to 10 seconds

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -2,5 +2,5 @@ import axios, { AxiosInstance } from "axios";
 
 export const client: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_BACKEND_URL,
-    timeout: 30000,
+    timeout: 10000,
 });
