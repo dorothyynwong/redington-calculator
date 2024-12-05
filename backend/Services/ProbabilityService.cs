a761096bc1b4924705731a63e5809f98ee3d60ee
dorothyynwong <dorothyynwong@gmail.com>
2024-12-05 14:52:26 +0000

    refactored ProbabilityService

--- backend/Services/ProbabilityService.cs
+++ backend/Services/ProbabilityService.cs
@@ -20,28 +20,40 @@ namespace RedingtonCalculator.Services
                 { ProbabilityOperation.Given, givenOperation }
             };
         }
-
         public decimal Calculate(decimal num1, decimal num2, ProbabilityOperation operation)
+        {
+            ValidateProbabilities(num1, num2);
+            var selectedOperation = GetOperation(operation);
+            return CalculateResult(num1, num2, operation, selectedOperation);
+        }
+
+        private void ValidateProbabilities(decimal num1, decimal num2)
         {
             if (num1 < 0 || num1 > 1 || num2 < 0 || num2 > 1)
             {
-                var ex = new ArgumentOutOfRangeException("Probabilities must be between 0 and 1");
-                throw ex;
+            var ex = new ArgumentOutOfRangeException("Probabilities must be between 0 and 1");
+            throw ex;
             }
+        }
 
+        private IProbabilityCalculator GetOperation(ProbabilityOperation operation)
+        {
             if (_operations.ContainsKey(operation))
             {
-                var selectedOperation = _operations[operation];
-                decimal calculatedValue = selectedOperation.Execute(num1, num2);
-
-                _logger.Info($"Result for {operation} of {num1} and {num2} is {calculatedValue}");
-                return calculatedValue;
+            return _operations[operation];
             }
             else
             {
-                var ex = new ArgumentException("Invalid operation");
-                throw ex;
+            var ex = new ArgumentException("Invalid operation");
+            throw ex;
             }
+        }
+
+        private decimal CalculateResult(decimal num1, decimal num2, ProbabilityOperation operation, IProbabilityCalculator selectedOperation)
+        {
+            decimal calculatedValue = selectedOperation.Execute(num1, num2);
+            _logger.Info($"Result for {operation} of {num1} and {num2} is {calculatedValue}");
+            return calculatedValue;
         }
     }
 }
